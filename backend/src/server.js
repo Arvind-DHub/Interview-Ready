d79147488e9755040e06f7aaa69721f4143080db
Arvind-DHub <arvinddharwad7@gmail.com>
2026-03-12 11:40:03 +0530

    session routes & controllkers added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,53 +1,57 @@
-import dns from 'dns';
-// Set DNS servers before any network code 
-dns.setServers(['8.8.8.8', '1.1.1.1']);
-//solves ECONNREFUSED error due to change in the c-ares library the DNS fallback detection bug in Node
-//const express = require('express') usig module type 
-import express from "express"
-import {ENV} from "./lib/env.js"
-import path from "path"
+import dns from "dns";
+// Set DNS servers before any network code
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+// solves ECONNREFUSED error due to change in the c-ares library the DNS fallback detection bug in Node
+// const express = require('express') usig module type
+import express from "express";
+import { ENV } from "./lib/env.js";
+import path from "path";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
-import {serve} from "inngest/express";
-import { inngest, functions } from './lib/inngest.js';
-import { clerkMiddleware } from '@clerk/express'
-import chatRoutes from "./routes/chatRoutes.js"
+import { serve } from "inngest/express";
+import { inngest, functions } from "./lib/inngest.js";
+import { clerkMiddleware } from "@clerk/express";
+import chatRoutes from "./routes/chatRoutes.js";
+import sessionRoutes from "./routes/sessionRoute.js";
 
 const app = express();
 
 const __dirname = path.resolve();
 
-app.use(express.json())
-//credentials:true meaning?? => server allows browser to include cookies on request
-app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+app.use(express.json());
+// credentials:true meaning?? => server allows browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 
 app.use(clerkMiddleware()); // this adds auth field to req obj : req.auth()
 
-app.use("/api/inngest", serve({client:inngest, functions}));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
-app.use("/api/chat", chatRoutes)
+app.use("/api/chat", chatRoutes);
+
+app.use("/api/sessions", sessionRoutes);
 
 app.get("/health", (req, res) => {
-    res.status(200).json({msg:"api is up and running"})
-})
+  res.status(200).json({ msg: "api is up and running" });
+});
 
-//make our app ready for deployment
-if(ENV.NODE_ENV == "production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+// make our app ready for deployment
+if (ENV.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("/{*any}", (req,res) => {
-        res.sendFile(path.join(__dirname,"../frontend", "dist", "index.html"));
-    })
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-const startServer = async() => {
-    try{
-        await connectDB();
-        app.listen(ENV.PORT, "0.0.0.0", ()=> {
-        console.log("👌 Server is running from port:", ENV.PORT); })
-    }catch(error){
-        console.error("💥 Error Starting the server ",error);
-    }
-}
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, "0.0.0.0", () => {
+      console.log("👌 Server is running from port:", ENV.PORT);
+    });
+  } catch (error) {
+    console.error("💥 Error Starting the server ", error);
+  }
+};
 
 startServer();
